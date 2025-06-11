33716c08c8825462b4b41d0b312dccdd887d7276
sanjjuo <sanjeedkoraliyil29399@gmail.com>
2025-06-11 15:11:18 +0400

    third commit

--- app/(main)/products/[productId]/page.tsx
+++ app/(main)/products/[productId]/page.tsx
@@ -1,10 +1,10 @@
 import ProductDetailsClient from '@/components/ProductDetailsClient/ProductDetailsClient';
 import { fetchProductDetails } from '@/services/fetchProducts';
 
-const ProductDetails = async ({ params }: { params: { productId: string } }) => {
-  const { productId } = await params;
-    const productDetails = await fetchProductDetails(productId);
-    return <ProductDetailsClient productDetails={productDetails} />;
+const ProductDetails = async ({ params: { productId } }: { params: { productId: string } }) => {
+  const ProdId = await productId;
+  const productDetails = await fetchProductDetails(ProdId);
+  return <ProductDetailsClient productDetails={productDetails} />;
 };
 
 export default ProductDetails;
